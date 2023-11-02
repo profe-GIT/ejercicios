ae2b8059df78672425426e8f8a9a77f5fe33d07d
Roberto <Roberto@DESKTOP-5FL7IBM>
2023-11-02 21:40:38 +0100

    versión comentada

--- pruebaGit/src/pruebaGit/pruebaGit.java
+++ pruebaGit/src/pruebaGit/pruebaGit.java
@@ -5,6 +5,7 @@ public class pruebaGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("esto es una prueba de GIT");
+		//prueba  de  sacar por pantalla
 	}
 
 }
