288f1b258cd7961b62e4f65b9dcae9e603f68374
Roberto <Roberto@DESKTOP-5FL7IBM>
2023-11-02 21:34:42 +0100

    version inicial

---
+++ pruebaGit/src/pruebaGit/pruebaGit.java
@@ -0,0 +1,10 @@
+package pruebaGit;
+
+public class pruebaGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("esto es una prueba de GIT");
+	}
+
+}
